1726bf35395eb072e210a29ef0ed7f8614d27e3c
mattiasfossecave <mattfossecave@gmail.com>
2023-10-15 10:03:19 +0200

    Update register.php

--- src/php/register.php
+++ src/php/register.php
@@ -3,6 +3,34 @@ require_once('bdd.php');
 require_once('fonctions.php');
 //var_dump($_POST);
 
+if(isset($_POST['send'])){
+    if(!empty($_POST['username']) && !empty($_POST['mdp']) && !empty($_POST['email']) ){
+        // strip_tags pour enlever le code html sql etc etc 
+        $username = strip_tags($_POST['username']);
+        $email = strip_tags($_POST['email']);
+        $mdp = password_hash($_POST['mdp'],PASSWORD_DEFAULT);
+
+        $insertUser = $db-> prepare('INSERT INTO list_user(UserName,Password,Email)VALUES(?,?,?)');
+        $insertUser->execute(array($username,$mdp,$email));
+        
+        $getUser = $db->prepare('SELECT * FROM list_user WHERE UserName = ? AND Password = ? AND Email = ?');
+        $getUser->execute(array($username,$mdp,$email));
+
+        if($getUser->rowCount()>0){
+            $_SESSION['Username'] = $username;
+            $_SESSION['id'] = $getUser->fetch()['Id_list_user'];
+            header("Location: ../HTML/identification.php");
+            exit();
+        }
+
+        echo $_SESSION['id'];
+
+    }else{
+        echo "veuillez saisir tout pd";
+    }
+}
+
+
 ?>
 
 <!-- <script type='text/javascript'>alert("heaejhazilejai");</script> -->
@@ -17,7 +45,7 @@ require_once('fonctions.php');
     </head>
     <body>
 <main>
-    <form action="inscriptionValidation.php" method="POST">
+    <form action="" method="POST">
         <h2>Inscription</h2>
         <div>
             <label>Email:</label>
@@ -41,7 +69,7 @@ require_once('fonctions.php');
             <label>Confimer le mot de passe:</label>
             <input type="password" name="mdp2">
         </div>
-        <button type="submit">S'inscrire</button>
+        <button name="send" type="submit">S'inscrire</button>
         <footer>Déjà Membre ? <a href="login.php">Connectez-vous</a></footer>
     </form>
 </main>
