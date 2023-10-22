b232065e1c4f3f51681e86044f7fa571a608de55
DavidLaclef <daev.cognita@gmail.com>
2023-10-22 08:18:02 +0200

    API finie ouuuu ?

---
+++ src/api/read_all.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('../php/bdd.php');
+
+
+function read_all($db)
+{
+	$query = 'SELECT * FROM matchs';
+	$return = $db->query($query);
+	$result = [];
+
+	while ($row = $return->fetch(PDO::FETCH_ASSOC)) {
+		array_push($result, $row);
+	}
+	$return->closeCursor();
+
+	return json_encode($result);
+}
+
+header('Content-Type: application/json');
+echo read_all($db);
