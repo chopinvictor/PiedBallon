ad824c83831b812509e5b1c23ca2862921d14d1d
mattiasfossecave <mattfossecave@gmail.com>
2023-10-25 11:31:10 +0200

    ennnncore

--- src/php/match_create.php
+++ src/php/match_create.php
@@ -3,6 +3,18 @@
 session_start();
 require_once('./bdd.php');
 
+
+if(empty($_SESSION) || ($_SESSION['admin']!==1)){
+    header("Location: ../php/login.php");
+}
+
+
+$query = "SELECT * FROM equipes";
+$getmatch = $db->prepare($query);
+$getmatch->execute();
+
+$equipe= $getmatch->fetchAll(PDO::FETCH_OBJ);
+
 // colonne 'est_fini' dans la table matchs
 $estFini;
 
@@ -24,12 +36,46 @@ function create($db, $date_match, $lieu_match, $score_equipe_1, $score_equipe_2,
 		)
 	);
 
+
+	$query = "SELECT * 
+	FROM matchs
+	WHERE date_match = ? AND lieu_match = ?";
+	$getmatch = $db->prepare($query);
+	$getmatch->execute(array($date_match, $lieu_match));
+
+	$id= $getmatch->fetchAll(PDO::FETCH_OBJ);
+	$id_match = $id[0]->id_match;
+
+	$query = "INSERT INTO equipe_joue(id_match,id_equipe)
+	VALUES (?,?)";
+  	$return = $db->prepare($query);
+  	$return->execute(
+	  array(
+		  
+		  $id_match,$_POST['equipe_une'],
+	  )
+  );
+
+  $query = "INSERT INTO equipe_joue(id_match,id_equipe)
+  VALUES (?,?)";
+
+	$return = $db->prepare($query);
+
+
+	$return->execute(
+	array(
+		
+		$id_match,$_POST['equipe_deux'],
+	)
+);
+
 	$return->closeCursor();
+	header("Location: ../php/club.php");
 }
 
 if (isset($_POST['send'])) {
 	if (
-		!empty($_POST['lieu_match']) && !empty($_POST['date_match']) && !empty($_POST['score_equipe_1']) && !empty($_POST['score_equipe_2']) && !empty($_POST['est_fini'])
+		!empty($_POST['lieu_match']) && !empty($_POST['date_match']) && !empty($_POST['est_fini'])
 	) {
 		$estFini = $_POST['est_fini'];
 
@@ -69,6 +115,31 @@ if (isset($_POST['send'])) {
 			<label class="form-row">Match terminé</label>
 			<input type="checkbox" name="est_fini" value="1" />
 		</div>
+		<div class="name_player">
+			<label for="equipe_une" >equipe 1</label>
+				<select name="equipe_une" id="select_team">
+					
+					<?php
+                foreach($equipe as $Value) : ;
+                    $nom = $Value->nom_equipe;
+					$id=$Value->id_equipe ; ?>
+					<option value="<?= $id?>"><?= $nom?></option>
+            <?php endforeach ?>
+				</select>
+		</div>
+		<div class="name_player">
+			<label for="equipe_deux" >equipe 2</label>
+				<select name="equipe_deux" id="select_team">
+					
+					<?php
+                foreach($equipe as $Value) : ;
+                    $nom = $Value->nom_equipe;
+					$id=$Value->id_equipe ; ?>
+					<option value=<?= $id?>><?= $nom?></option>
+            <?php endforeach ?>
+				</select>
+		</div>
+
 
 		<button name="send" type="submit">Créer</button>
 
